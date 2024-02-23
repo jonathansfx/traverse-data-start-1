711901308fba308d5a88195207c564c7a696a1d7
jonathan <jonathanluo22@gmail.com>
2024-02-23 09:59:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-# opening files and stuff
-age_data_file = open("age-data.txt","r")
-number_data_file = open("number-data.txt", "r")
-survey_results_file = open("survey-results.txt", "r")
-
-# reading the file
-age_data = age_data_file.read()
-number_data = number_data_file.read()
-survey_results = survey_results_file.read()
-
 # age data
+with open('age-data.txt','r') as age_data_file:
+    age_data = age_data_file.read().split('\n')
+    
 underage = 0
-youngadult = 0
+young_adult = 0
 adult = 0
 senior = 0
-for x in age_data:
-    print(x)
+for x in range(0, len(age_data)):
+    age_data[x] = int(age_data[x])
+    if age_data[x] < 18:
+        underage += 1
+    elif age_data[x] > 18 and age_data[x] < 36:
+        young_adult += 1
+    elif age_data[x] > 35 and age_data[x] < 66:
+        adult += 1
+
+print(underage"," young_adult)
+
+# number data
+with open ('number-data.txt', 'r') as number_data_file:
+    number_data = number_data_file.read().split('\n')
+
