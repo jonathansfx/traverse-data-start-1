84694ba8ce0cdef3b5cb5c88047898ddd52eafde
jonathan <jonathanluo22@gmail.com>
2024-02-28 09:11:57 -0700

    still need to print everything out

--- main.py
+++ main.py
@@ -10,14 +10,40 @@ for x in range(0, len(age_data)):
     age_data[x] = int(age_data[x])
     if age_data[x] < 18:
         underage += 1
-    elif age_data[x] > 18 and age_data[x] < 36:
+    elif age_data[x] >= 18 and age_data[x] <= 35:
         young_adult += 1
     elif age_data[x] > 35 and age_data[x] < 66:
         adult += 1
 
-print(underage"," young_adult)
+
 
 # number data
 with open ('number-data.txt', 'r') as number_data_file:
     number_data = number_data_file.read().split('\n')
 
+even_numbers = 0
+odd_numbers = 0
+
+for x in range(0, len(number_data)):
+    number_data[x] = int(number_data[x])
+    if number_data[x] % 2 == 0:
+        even_numbers += 1
+    else:
+        odd_numbers += 1
+
+
+
+# survey data
+with open ('survey-results.txt', 'r') as survey_results_file:
+    survey_results = survey_results_file.read().split('/n')
+
+no_response = 0
+yes_response = 0
+
+for x in range(0, len(survey_results)):
+    if survey_results[x] == "yes":
+        yes_response += 1
+    elif survey_results[x] == "no":
+        no_response += 1
+
+print(f"{underage} {young_adult} {adult} {even_numbers} {odd_numbers}" )
